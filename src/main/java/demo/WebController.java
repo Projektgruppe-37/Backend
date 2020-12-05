dff55dbd8c018e23c3619f90b7f7c7992ce53b47
mikail <micen19@student.sdu.dk>
2020-12-05 01:04:09 +0100

    Startup live data

--- src/main/java/demo/WebController.java
+++ src/main/java/demo/WebController.java
@@ -1,7 +1,11 @@
 package demo;
 
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sdu.beermachine.Read;
+import sdu.beermachine.Write;
 
 @Controller
 public class WebController {
@@ -9,5 +13,10 @@ public class WebController {
     @GetMapping("/")
     public String homePage() {
         return "index";
+    }
+
+    @GetMapping("/machine_speed")
+    public void machine_speed() {
+        Write.setMachSpeed(300);
     }
 }
