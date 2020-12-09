f96149d4331664867a2c224151b4f0c1ab9cb170
mara916 <mara816@student.sdu.dk>
2020-12-09 10:37:39 +0100

    removed configUa for each class

--- src/main/java/demo/service/SetValues.java
+++ src/main/java/demo/service/SetValues.java
@@ -1,5 +1,6 @@
 package demo.service;
 
+import demo.model.ConfigUa;
 import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
 import org.eclipse.milo.opcua.stack.client.DiscoveryClient;
@@ -12,101 +13,32 @@ import org.eclipse.milo.opcua.stack.core.util.EndpointUtil;
 
 import java.util.List;
 
-public class SetValues {
+public class SetValues extends ConfigUa {
 
-    private static OpcUaClient client;
 
     public void setMachSpeed(float valMS) {
-        try {
 
-            List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://127.0.0.1:4840").get();
-            //System.out.println("Endpoints = " + endpoints);
-
-
-            EndpointDescription configPoint = EndpointUtil.updateUrl(endpoints.get(0), "127.0.0.1", 4840);
-
-            OpcUaClientConfigBuilder cfg = new OpcUaClientConfigBuilder();
-            cfg.setEndpoint(configPoint);
-            // cfg.setEndpoint(endpoints.get(0));
-
-            client = OpcUaClient.create(cfg.build());
-            client.connect().get();
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
         System.out.println("MachSpeed = " + valMS);
         NodeId nodeIdMS = new NodeId(6, "::Program:Cube.Command.MachSpeed");
         client.writeValue(nodeIdMS, DataValue.valueOnly(new Variant(valMS)));
     }
 
     public void setProductAmount(float valP2) {
-        try {
 
-            List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://127.0.0.1:4840").get();
-            //System.out.println("Endpoints = " + endpoints);
-
-
-            EndpointDescription configPoint = EndpointUtil.updateUrl(endpoints.get(0), "127.0.0.1", 4840);
-
-            OpcUaClientConfigBuilder cfg = new OpcUaClientConfigBuilder();
-            cfg.setEndpoint(configPoint);
-            // cfg.setEndpoint(endpoints.get(0));
-
-            client = OpcUaClient.create(cfg.build());
-            client.connect().get();
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
         NodeId nodeIdP2 = new NodeId(6, "::Program:Cube.Command.Parameter[2].Value");
         client.writeValue(nodeIdP2, DataValue.valueOnly(new Variant(valP2)));
         System.out.println("Parameter 2 = " + valP2);
     }
 
     public void setProductType(float valP1) {
-        try {
 
-            List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://127.0.0.1:4840").get();
-            //System.out.println("Endpoints = " + endpoints);
-
-
-            EndpointDescription configPoint = EndpointUtil.updateUrl(endpoints.get(0), "127.0.0.1", 4840);
-
-            OpcUaClientConfigBuilder cfg = new OpcUaClientConfigBuilder();
-            cfg.setEndpoint(configPoint);
-            // cfg.setEndpoint(endpoints.get(0));
-
-            client = OpcUaClient.create(cfg.build());
-            client.connect().get();
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
         NodeId nodeIdP1 = new NodeId(6, "::Program:Cube.Command.Parameter[1].Value");
         client.writeValue(nodeIdP1, DataValue.valueOnly(new Variant(valP1)));
         System.out.println("Parameter 1 = " + valP1);
     }
 
     public void setCntrlCmd(int valCC) {
-        try {
 
-            List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://127.0.0.1:4840").get();
-            //System.out.println("Endpoints = " + endpoints);
-
-
-            EndpointDescription configPoint = EndpointUtil.updateUrl(endpoints.get(0), "127.0.0.1", 4840);
-
-            OpcUaClientConfigBuilder cfg = new OpcUaClientConfigBuilder();
-            cfg.setEndpoint(configPoint);
-            // cfg.setEndpoint(endpoints.get(0));
-
-            client = OpcUaClient.create(cfg.build());
-            client.connect().get();
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
         NodeId nodeIdCC = new NodeId(6, "::Program:Cube.Command.CntrlCmd");
         client.writeValue(nodeIdCC, DataValue.valueOnly(new Variant(valCC)));
         System.out.println("CntrlCmd = " + valCC);
