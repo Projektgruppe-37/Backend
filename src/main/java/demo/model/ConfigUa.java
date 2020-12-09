acf221587caef99ff57b8dde4c5438241d2d0cef
Mikail Badad <mikailcengiz@live.dk>
2020-12-09 10:01:25 +0100

    Changes

---
+++ src/main/java/demo/model/ConfigUa.java
@@ -0,0 +1,33 @@
+package demo.model;
+
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
+import org.eclipse.milo.opcua.stack.client.DiscoveryClient;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+import org.eclipse.milo.opcua.stack.core.util.EndpointUtil;
+
+import java.util.List;
+
+public abstract class ConfigUa {
+
+    public static OpcUaClient client;
+
+    public static void configUa() {
+
+        try {
+
+            List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://127.0.0.1:4840").get();
+
+            EndpointDescription configPoint = EndpointUtil.updateUrl(endpoints.get(0), "127.0.0.1", 4840);
+
+            OpcUaClientConfigBuilder cfg = new OpcUaClientConfigBuilder();
+            cfg.setEndpoint(configPoint);
+
+            client = OpcUaClient.create(cfg.build());
+            client.connect().get();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
